e0070bdc26ac5bd875e4bd7106b1ffa769dbad85
HLII0247 <hlii0247@student.monash.edu>
2023-08-29 13:01:47 +1000

    week6

---
+++ week6/week6/Models/SampleFormViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+namespace week6.Models
+{
+    public class SampleFormViewModels
+    {
+    }
+
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name = "UserName")]
+        
+        public string UserName { get; set; }
+        [Required]  
+        [Display(Name = "PassWord")] 
+        public string PassWord { get; set; }
+    }
+}
---
+++ week6/week6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(week6.Startup))]
+namespace week6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
